5747a6b8d56789ba7535ecb8e15a248d45c1f24c
NazmulHasanNahin <nahinn75@gmail.com>
2025-04-17 22:42:49 +0600

    post get

--- index.js
+++ index.js
@@ -13,6 +13,87 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@skypath-react-db.odx7eii.mongodb.net/?retryWrites=true&w=majority&appName=skypath-react-db`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+    // all country er data fetch 
+    const allCountry = client.db("skypathDB").collection("countries")
+
+
+    // get 
+
+    app.get("/countries", async (req, res) => {
+      const cursor = allCountry.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+
+
+    //all spot er data pathanor jnne 
+
+    const touristSpotCollection = client.db("skypathDB").collection("touristSpots")
+
+
+    //DATA post korar jnne spot gula 
+    app.post("/add-spots", async (req, res) => {
+      const spotData = req.body;
+      const result = await touristSpotCollection.insertOne(spotData);
+      res.send(result);
+    });
+
+    // country wise data filter korar jnne 
+    app.get("/spots/:country", async (req, res) => {
+      const country = req.params.country;
+      const spots = await touristSpotCollection.find({ country_Name: country }).toArray();
+      res.send(spots);
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB and server is running!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 
@@ -21,10 +102,10 @@ app.use(express.json());
 
 
 app.get("/", (req, res) => {
-    res.send(`travel site server running ${port} port`)
+  res.send(`travel site server running ${port} port`)
 })
 
 
 app.listen(port, () => {
-    console.log(`travel site  server is running on port: ${port}`);
+  console.log(`travel site  server is running on port: ${port}`);
 })
